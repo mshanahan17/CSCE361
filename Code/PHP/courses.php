31336cb7fc2dfc8f9239937ae0db82235522bf65
Alejandro David Pages <Alpacapages@gmail.com>
2018-03-17 11:38:45 -0500

    Update courses.php

--- Code/PHP/courses.php
+++ Code/PHP/courses.php
@@ -6,7 +6,11 @@
 
 		$sql = new mysqli("cse.unl.edu", "apages", "Wtpt4R", "apages");
 
-		$qry = "SELECT c.title, c.description, c.prerequisite, c.creditHours, c.courseNum, a1.aceNum, a2.aceNum FROM Course c JOIN Subject ON c.subject_fk = Subject.subject_key LEFT JOIN Ace AS a1 ON c.ace_1_fk = a1.ace_key LEFT JOIN Ace AS a2 ON c.ace_2_fk = a2.ace_key WHERE subjectId = ?";
+		$qry = "SELECT c.title, c.description, c.prerequisite, c.creditHours, c.courseNum, a1.aceNum, a2.aceNum ".
+		       "FROM Course c JOIN Subject ON c.subject_fk = Subject.subject_key ".
+		       "LEFT JOIN Ace AS a1 ON c.ace_1_fk = a1.ace_key ".
+		       "LEFT JOIN Ace AS a2 ON c.ace_2_fk = a2.ace_key ".
+		       "WHERE subjectId = ?";
 		
 		$qry = $sql->prepare($qry);
 		$qry->bind_param("s", $code);
