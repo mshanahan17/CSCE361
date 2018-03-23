b7a4d57dc1d2b34ba1b617fb20cfd6777e2aa734
Matthew J Shanahan <matthew.shanahan@huskers.unl.edu>
2018-03-23 10:50:29 -0500

    Update courses.php

--- Code/PHP/courses.php
+++ Code/PHP/courses.php
@@ -1,30 +1,35 @@
 <?php
-//error_reporting(E_ALL); ini_set('display_errors', 1);
-$search = json_decode($_POST['search']);
-$search1 = "%".$search[0]."%";
-$search2 = "%".$search[1]."%";
-$sql = new mysqli("cse.unl.edu", "apages", "Wtpt4R", "apages");
-$qry = "SELECT c.title, c.description, c.prerequisite, c.creditHours, c.courseNum, s.subjectId, a1.aceNum AS aceNum1, a2.aceNum AS aceNum2 ".
-    "FROM Course c JOIN Subject s ON c.subject_fk = s.subject_key ".
-    "LEFT JOIN Ace AS a1 ON c.ace_1_fk = a1.ace_key ".
-    "LEFT JOIN Ace AS a2 ON c.ace_2_fk = a2.ace_key ".
-    "WHERE UPPER(s.subjectId) LIKE UPPER(?) AND c.courseNum LIKE ?";
-$qry = $sql->prepare($qry);
-$qry->bind_param('ss', $search1, $search2);
-$qry->execute();
-$result = $qry->get_result();
-$sql->close();
-while ($row = mysqli_fetch_assoc($result)) {
-    $rows[] = array(
-        'title' => $row['title'],
-        'description' => $row['description'],
-        'prerequisite' => $row['prerequisite'],
-        'creditHours' => $row['creditHours'],
-        'courseNum' => $row['courseNum'],
-        'courseName' => $row['subjectId'],
-        'ace1' => $row['aceNum1'],
-        'ace2' => $row['aceNum2']
-    );
+
+$codes = json_decode($_POST['codes']);
+$rows = array();
+foreach($codes as $code) {
+    $sql = new mysqli("cse.unl.edu", "apages", "Wtpt4R", "apages");
+    $qry = "SELECT c.title, c.description, c.prerequisite, c.creditHours, c.courseNum, s.subjectId, a1.aceNum AS aceNum1, a2.aceNum AS aceNum2 ".
+        "FROM Course c JOIN Subject s ON c.subject_fk = s.subject_key ".
+        "LEFT JOIN Ace AS a1 ON c.ace_1_fk = a1.ace_key ".
+        "LEFT JOIN Ace AS a2 ON c.ace_2_fk = a2.ace_key ".
+        "WHERE subjectId = ?";
+
+    $qry = $sql->prepare($qry);
+    $qry->bind_param("s", $code);
+    $qry->execute();
+    $result = $qry->get_result();
+    //while($row = mysqli_fetch_assoc($result)) {
+    //	echo $row["title"];
+    //}
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = array(
+            'title' => $row['title'],
+            'description' => $row['description'],
+            'prerequisite' => $row['prerequisite'],
+            'creditHours' => $row['creditHours'],
+            'courseName' => $row['subjectId'],
+            'courseNum' => $row['courseNum'],
+            'ace1' => $row['aceNum1'],
+            'ace2' => $row['aceNum2']
+        );
+    }
 }
+mysqli_close($sql);
 echo json_encode($rows);
 ?>
